bd369709b12ffed597c23edf4d2d08b0f62a8eb5
Ekpo Winners <danielemmanuel749@gmail.com>
2025-09-03 23:53:07 +0100

    new

--- streamlitapp/app.py
+++ streamlitapp/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import os
+import sklearn
 
 def get_clean_data():
     csv_path = os.path.join(os.path.dirname(__file__), "data.csv")
