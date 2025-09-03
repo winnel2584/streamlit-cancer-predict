3471088e75dcee6cc094ad04bc057c799852afc7
Ekpo Winners <danielemmanuel749@gmail.com>
2025-09-03 23:09:03 +0100

    new fix

--- streamlitapp/app.py
+++ streamlitapp/app.py
@@ -166,7 +166,7 @@ def main():
         initial_sidebar_state="expanded" 
     )
 
-    with open("styles.css") as f:
+    with open("streamlitapp/styles.css") as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
     input_data = add_sidebar()
